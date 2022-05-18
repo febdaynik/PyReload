14866960be08de2e57570828bc3090592fbfecb5
feb day <drons_dron@mail.ru>
2022-05-19 02:41:55 +0400

    fix and update script and eeadme

--- README.md
+++ README.md
@@ -4,19 +4,36 @@ PyReload
 Библиотека для быстрого взаимодействия с кодом в файле.
 PyReload - позволяет обновлять код в реальном времени
 
-Examples
-----
+Конфигурация
+====
 
-## For aiogram
+Чтобы не следить за всеми файлами их можно поместить в файл игнорирования, по стандарту он '._ingore'. Для этого вам понадобиться:
+```
+1. Создать файл игнорирования
+2. Добавить игнорируемые файлы
+3. Запустить скрипт (если файл называется иначе, то передайте его название через параметр 'ignore_file')
+
+ps: Создание файл не является обязательным
+```
+
+Примеры
+====
+
+## Для aiogram
 
 ```python
 # update code in framework aiogram
 
 import os
 from pyreload import PyReload
-async def on_startup(x):
-	PyReload(os.listdir()).update_file_hash()
+async def _reload():
+	pr.update_file.hash().run()
+
+def repeat(coro, loop):
+	asyncio.ensure_future(coro(), loop=loop)
+	loop.call_later(1, repeat, coro, loop)
 
 if __name__ == '__main__':
-	executor.start_polling(dp, skip_updates=True, on_startup=on_startup)   
+	pr = PyReload(path=os.listdir())
+	executor.start_polling(dp, skip_updates=True, loop=loop)   
 ```
--- pyreload/__init__.py
+++ pyreload/__init__.py
@@ -2,33 +2,30 @@ import logging
 import os, sys, time
 import hashlib
 
-# logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 
 class PyReload:
-	def __init__(self, path = list()):
+	def __init__(self, ingore_file: str = "._ignore", path: list() = [os.path.basename(__file__)]):
 		if type(path) != list:
 			self.path = [path]
 		else:
 			self.path = path
 
-		self._ignore()
-		
+		self._ignore(ingore_file)
+
 		self.timestamp = self.get_timestamp()
 		self.hash_text = [{file:self.get_hash_text(path=file)} for file in self.path]
 		logging.info("[+] Started PyReload")
 
-	def _ignore(self):
-		with open("._ignore", encoding="utf-8") as conf_ign: ign = conf_ign.readlines()
-		[ self.path.remove(file.replace("\n","")) for file in ign ]
-
-	def get_timestamp(self):
-		try:
-			return [{file:os.stat(file).st_mtime} for file in self.path]
-		except FileNotFoundError as e: 
-			logging.error(f"[-] Файл {e.filename} не был найден")
-			self.path.remove(e.filename)
-
+	def _ignore(self, ingore_file):
+		with open(ingore_file, encoding="utf-8") as conf_ign: ign = conf_ign.readlines()
+		# [ self.path.remove(file.replace("\n","")) for file in ign ]
+		for file in ign:
+			try:
+				self.path.remove(file.replace("\n",""))
+			except FileNotFoundError: pass
+			except ValueError: pass
 
 	def encode_md5(self, data):
 		h = hashlib.md5(data)
@@ -39,32 +36,53 @@ class PyReload:
 			with open(path, "rb") as file:
 				return self.encode_md5(file.read())
 		except FileNotFoundError as e: 
-			logging.error(f"[-] Файл {path} не был найден")
+			logging.error(f"Файл {path} не был найден")
 			self.path.remove(e.filename)
 
-	# Проверяет обновление файлов исходя из их содержания
-	def update_file_hash(self):
-		while True:
-			for name,index in zip(self.path, range(len(self.path))): 
-				text_hash = self.get_hash_text(path=name)
-				# print(self.hash_text[index][name])
-				if self.hash_text[index][name] != text_hash:
-					self.hash_text[index][name] = text_hash
-					logging.info("[Update] Файл был изменён!")
-					self._reload_script_()
-
-			# Задержка стоит для того, чтобы файл два раза не сохранялся (не знаю почему так работает)
-			time.sleep(.1)
-
-	# Проверяет обновление файлов исходя из времени обновления
-	def update_file_timestamp(self):
-		while True:
-			for name,index in zip(self.path, range(len(self.path))):
-				if self.timestamp[index][name] != os.stat(name).st_mtime:
-					self.timestamp[index][name] = os.stat(name).st_mtime
-					logging.info("[Update timestamp] Файл был изменён!")
-					self._reload_script_()
+	def get_timestamp(self):
+		try:
+			return [{file:os.stat(file).st_mtime} for file in self.path]
+		except FileNotFoundError as e: 
+			logging.error(f"Файл {e.filename} не был найден")
+			self.path.remove(e.filename)
+	
+	@property
+	def update_file(self):
+		return UpdateFile(self)
 
 	def _reload_script_(self):
 		logging.info("[+] Reloading script")
 		os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+
+class UpdateFile:
+	def __init__(self, pr: PyReload = None):
+		self.pr = pr
+		self.func = None
+
+	# Проверяет обновление файлов исходя из их содержания
+	def hash(self):
+		self.func = "hash"
+		for name,index in zip(self.pr.path, range(len(self.pr.path))): 
+			text_hash = self.pr.get_hash_text(path=name)
+			if self.pr.hash_text[index][name] != text_hash:
+				self.pr.hash_text[index][name] = text_hash
+				logging.info("[Update] Файл был изменён!")
+				self.pr._reload_script_()
+
+		return self
+
+	# Проверяет обновление файлов исходя из времени обновления
+	def timestamp(self):
+		self.func = "timestamp"
+		for name,index in zip(self.pr.path, range(len(self.pr.path))):
+			if self.pr.timestamp[index][name] != os.stat(name).st_mtime:
+				self.pr.timestamp[index][name] = os.stat(name).st_mtime
+				logging.info("[Update timestamp] Файл был изменён!")
+				self.pr._reload_script_()
+		return self
+
+	def run(self):
+		eval("self.%s()" % self.func)
+		time.sleep(.1)
